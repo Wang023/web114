635d5e8e8b4be830c8382214ca3a857d7ecc19e0
Gana_023 <105679129+Wang023@users.noreply.github.com>
2025-04-08 12:12:39 -0500

    Update password.js

--- ch4_extra/password.js
+++ ch4_extra/password.js
@@ -5,7 +5,7 @@ let password = prompt("Ebter your password ?");
 //chock for password criteria
 if (password.length >= 8 && password.match(/[A-Z]/) && password.match(/[a-z]/) && password.match(/\d/)){
     alert("Success! You've unlocked the secret message.");
-    console.log("SECRET MESSAGE: The future is infront of you...");
+    console.log("SECRET MESSAGE: Decryption complete. Welcome, Agent. Your next mission awaits...");
 } else {
     alert("Access Denied! Your password doesn't meet the requirements.");
     console.log("Password validation failed...");
