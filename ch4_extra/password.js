907dc09004757db1395e8bab6ee7a4f7653ee1cd
Gana_023 <105679129+Wang023@users.noreply.github.com>
2025-04-08 12:08:56 -0500

    Add files via upload

---
+++ ch4_extra/password.js
@@ -0,0 +1,12 @@
+//Gana Sehaki 04/08/2025
+//stock the user input in the password variable
+let password = prompt("Ebter your password ?");
+
+//chock for password criteria
+if (password.length >= 8 && password.match(/[A-Z]/) && password.match(/[a-z]/) && password.match(/\d/)){
+    alert("Success! You've unlocked the secret message.");
+    console.log("SECRET MESSAGE: The future is infront of you...");
+} else {
+    alert("Access Denied! Your password doesn't meet the requirements.");
+    console.log("Password validation failed...");
+}
